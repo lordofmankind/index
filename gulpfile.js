517a92341cccdf08bb645e859dc1d92c0dc9fcc4
lordofmankind <48515189+lordofmankind@users.noreply.github.com>
2020-03-03 21:34:37 +0300

    add index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
-const gulp = require("gulp");
-const cleanCSS = require("gulp-clean-css");
+const gulp = require ('gulp');
+const browserSync = require('browser-sync').create();
 
-function minifyCSS() {
-  return (
-    gulp
-      .src("./css/*.css")
-      .pipe(cleanCSS())
-      .pipe(gulp.dest("minified"))
-  );
-}
-
-gulp.task("minify-css", minifyCSS);
-
-gulp.task("watch", () => {
-  gulp.watch("./css/*.css", minifyCSS);
+gulp.task ('hello' , function(done)  {
+    console.log ('Привет, мир!');
+    done();
 });
 
-gulp.task('default', gulp.series('minify-css', 'watch'));
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
